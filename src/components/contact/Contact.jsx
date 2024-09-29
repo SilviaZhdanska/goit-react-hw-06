5f392c47ffdd23395a46b4ee4b1ec3b339277833
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-09-29 11:56:09 +0300

    updated hw

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
 
-export default function Contact({ contact, onDelete }) {
+export default function Contact({ contact }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+
   return (
     <li>
       {contact.name}: {contact.number}
-      <button onClick={onDelete}>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
